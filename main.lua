e855c43c91a1018eeca42223e43de4c5f43d5f99
jgrimaud <jgrimaud@student.42lyon.fr>
2024-09-07 04:03:53 +0200

    Removes ShowOnHover/ShowOnTarget (use the addon "Mouseover Actions Settings" instead). Fix a bug that occured when animation is enabled and the gcd is triggered.

--- main.lua
+++ main.lua
@@ -85,100 +85,23 @@ function OnReverseGrowDirChanged(bar)
 		button:SetPoint("CENTER", toContainer, "CENTER", 0, 0)
 	end
 end
+
 -- #endregion
 
 
 --#region -- Animation ---------------------------------------------------------
 
 function InitAnimations(bar)
-
-	function GetCooldown(button)
-		if not button.action then return 0 end
-		local start, duration, _, _ = GetActionCooldown(button.action)
-		if start <= 0 or duration <= 0 then return 0 end
-		return start + duration - GetTime()
-	end
-
-	-- Hide button components (keep the background visible)
-	function SetShowButton(button, show)
-		button.icon:SetAlpha(show and 1 or 0)
-		button.cooldown:SetAlpha(show and 1 or 0)
-		if button.chargeCooldown then
-			button.chargeCooldown:SetAlpha(show and 1 or 0)
-		end
-		if button.SpellActivationAlert then
-			button.SpellActivationAlert:SetAlpha(show and 1 or 0)
-		end
-		if button.Count then
-			button.Count:SetAlpha(show and 1 or 0)
-		end
-		if button.Name then
-			button.Name:SetAlpha(show and 1 or 0)
-		end
-	end
-
-	-- Move button elements (disallowed to move button in combat)
-	function SetButtonPosition(button, x, y)
-		button.icon:SetPoint("TOPLEFT", x, y)
-		button.icon:SetPoint("BOTTOMRIGHT", x, y)
-		button.cooldown:SetPoint("CENTER", x, y)
-		if button.chargeCooldown then
-			button.chargeCooldown:SetPoint("TOPLEFT", 2 + x, -2 + y)
-			button.chargeCooldown:SetPoint("BOTTOMRIGHT", -2 + x, 2 + y)
-		end
-		if button.SpellActivationAlert then
-			button.SpellActivationAlert:SetPoint("CENTER", x, y)
-		end
-		if button.Count then
-			button.Count:SetPoint("BOTTOMRIGHT", -5 + x, 5 + y)
-		end
-		if button.Name then
-			button.Name:SetPoint("BOTTOM", 0 + x, 2 + y)
-		end
-	end
-
 	for i = 1, 12 do
 		local button = _G[bar.buttonPrefix .. "Button" .. i]
 		if not button then return end
-		button.animate = false
-		-- Note: Not using OnShow because it is triggered by the gcd, hiding the cd.
+		ResetAnimation(button)
 		button.cooldown:HookScript("OnUpdate", function(self, elapsed)
-			if bar.animType == BarAnimType.none then return end
-			local cd = GetCooldown(button)
-			if cd > 5 then
-				button.animate = true
-			end
-			if button.animate then
-				if cd > 5 then
-					SetShowButton(button, false)
-				else
-					local x = 0
-					local y = 0
-					if bar.animType == BarAnimType.slideFromBottom then
-						x = 0
-						y = -cd * 20
-					elseif bar.animType == BarAnimType.slideFromTop then
-						x = 0
-						y = cd * 20
-					elseif bar.animType == BarAnimType.slideFromLeft then
-						x = -cd * 20
-						y = 0
-					elseif bar.animType == BarAnimType.slideFromRight then
-						x = cd * 20
-						y = 0
-					end
-					SetButtonPosition(button, x, y)
-					SetShowButton(button, true)
-				end
-			end
+			AnimateButton(bar, button, elapsed)
 		end)
-
 		button.cooldown:HookScript("OnHide", function(self)
-			button.animate = false
-			SetButtonPosition(button, 0, 0)
-			SetShowButton(button, true)
+			ResetAnimation(button)
 		end)
-
 	end
 end
 
@@ -186,9 +109,104 @@ function OnAnimTypeChanged(bar)
 	for i = 1, 12 do
 		local button = _G[bar.buttonPrefix .. "Button" .. i]
 		if not button then return end
-		button.animate = false
-		SetButtonPosition(button, 0, 0)
-		SetShowButton(button, true)
+		ResetAnimation(button)
+	end
+end
+
+function ResetAnimation(button)
+	button.cd = 0
+	button.animate = false
+	SetButtonPosition(button, 0, 0)
+	SetShowButton(button, true)
+end
+
+function AnimateButton(bar, button, elapsed)
+	if bar.animType == BarAnimType.none then return end
+
+	local cd = GetButtonCooldown(button)
+	if (cd > 1.5) then
+		button.cd = cd
+	else
+		-- If the cooldown is the gcd, manually decrease the cooldown to ignore it.
+		button.cd = button.cd - elapsed
+		if button.cd < 0 then
+			button.cd = 0
+			button.animate = false
+			return
+		end
+	end
+
+	if button.cd > 5 then
+		button.animate = true
+	end
+
+	if button.animate then
+		if button.cd > 5 then
+			SetShowButton(button, false)
+		else
+			local x = 0
+			local y = 0
+			if bar.animType == BarAnimType.slideFromBottom then
+				x = 0
+				y = -button.cd * 20
+			elseif bar.animType == BarAnimType.slideFromTop then
+				x = 0
+				y = button.cd * 20
+			elseif bar.animType == BarAnimType.slideFromLeft then
+				x = -button.cd * 20
+				y = 0
+			elseif bar.animType == BarAnimType.slideFromRight then
+				x = button.cd * 20
+				y = 0
+			end
+			SetButtonPosition(button, x, y)
+			SetShowButton(button, true)
+		end
+	end
+end
+
+function GetButtonCooldown(button)
+	if not button.action then return 0 end
+	local start, duration, _, _ = GetActionCooldown(button.action)
+	if start <= 0 or duration <= 0 then return 0 end
+	return start + duration - GetTime()
+end
+
+-- Hide button components (keep the background visible)
+function SetShowButton(button, show)
+	button.icon:SetAlpha(show and 1 or 0)
+	button.cooldown:SetAlpha(show and 1 or 0)
+	if button.chargeCooldown then
+		button.chargeCooldown:SetAlpha(show and 1 or 0)
+	end
+	if button.SpellActivationAlert then
+		button.SpellActivationAlert:SetAlpha(show and 1 or 0)
+	end
+	if button.Count then
+		button.Count:SetAlpha(show and 1 or 0)
+	end
+	if button.Name then
+		button.Name:SetAlpha(show and 1 or 0)
+	end
+end
+
+-- Move button elements (disallowed to move button in combat)
+function SetButtonPosition(button, x, y)
+	button.icon:SetPoint("TOPLEFT", x, y)
+	button.icon:SetPoint("BOTTOMRIGHT", x, y)
+	button.cooldown:SetPoint("CENTER", x, y)
+	if button.chargeCooldown then
+		button.chargeCooldown:SetPoint("TOPLEFT", 2 + x, -2 + y)
+		button.chargeCooldown:SetPoint("BOTTOMRIGHT", -2 + x, 2 + y)
+	end
+	if button.SpellActivationAlert then
+		button.SpellActivationAlert:SetPoint("CENTER", x, y)
+	end
+	if button.Count then
+		button.Count:SetPoint("BOTTOMRIGHT", -5 + x, 5 + y)
+	end
+	if button.Name then
+		button.Name:SetPoint("BOTTOM", 0 + x, 2 + y)
 	end
 end
 
